95086cf00afb1a32aa7a0aad446840f9474519b1
kalpesh <kalpeshbavaliya2005@gmail.com>
2024-10-30 21:49:28 +0530

    project structure

--- DFS/Project/banking.C
+++ DFS/Project/banking.C
@@ -5,12 +5,64 @@
 struct Bank
 {
     struct Bank *prev;
-
+    char name[100];
+    int acNO;
     struct Bank *next;
 };
+struct Bank typedefined node;
+node lp = NULL, rp = NULL;
+void create(node *);
+void deposite(node *);
+void withdrawal(node *);
+void exit(int);
 
 int main()
 {
+    int i = 1, choice;
 
+    while (i)
+    {
+        printf("/n Bank all operation are here ");
+
+        printf("/n 1. create a account 2. deposite balance 3. withdraw balance");
+
+        printf("/n enter a youtr choice ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            printf("/n create a bank account");
+            lp = (node *)malloc(sizeof(node));
+            create(lp);
+            break;
+        case 2:
+            printf("/n deposite balance");
+            deposite(lp);
+            break;
+
+        case 3:
+            printf("/n withdrawal balance");
+            withdrawal(lp);
+            break;
+        case 4:
+            printf("/n exite ");
+            exit(0);
+            break;
+        default:
+            printf("/n invalid choice ");
+            break;
+        }
+    }
     return 0;
+}
+
+void create(node *)
+{
+}
+void deposite(node *)
+{
+}
+void withdrawal(node *)
+{
 }
