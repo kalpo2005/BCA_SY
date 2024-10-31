33a4986a29509dc86d73974a5c6561e362ff6b36
kalpesh <kalpeshbavaliya2005@gmail.com>
2024-10-31 20:12:33 +0530

    create function done

--- DFS/Project/banking.C
+++ DFS/Project/banking.C
@@ -4,65 +4,85 @@
 
 struct Bank
 {
-    struct Bank *prev;
-    char name[100];
-    int acNO;
-    struct Bank *next;
+	struct Bank *prev;
+	char name[100];
+	int acNo, ammount, acType;
+	struct Bank *next;
 };
-struct Bank typedefined node;
-node lp = NULL, rp = NULL;
+struct Bank typedef node;
+node *lp = NULL, *rp = NULL;
 void create(node *);
 void deposite(node *);
 void withdrawal(node *);
-void exit(int);
 
 int main()
 {
-    int i = 1, choice;
+	int i = 1, choice;
 
-    while (i)
-    {
-        printf("/n Bank all operation are here ");
+	while (i)
+	{
+		printf("\n Bank all operation are here :");
 
-        printf("/n 1. create a account 2. deposite balance 3. withdraw balance");
+		printf("\n\n 01. create a account 02. deposite balance \n 03. withdraw balance 04. exite ...");
 
-        printf("/n enter a youtr choice ");
-        scanf("%d", &choice);
+		printf("\n \n enter a youtr choice :");
+		scanf("%d", &choice);
 
-        switch (choice)
-        {
-        case 1:
-            printf("/n create a bank account");
-            lp = (node *)malloc(sizeof(node));
-            create(lp);
-            break;
-        case 2:
-            printf("/n deposite balance");
-            deposite(lp);
-            break;
+		switch (choice)
+		{
+		case 1:
+			printf("\n Create a bank account");
+			lp = (node *)malloc(sizeof(node));
+			create(lp);
+			break;
+		case 2:
+			printf("\n Deposite balance");
+			deposite(lp);
+			break;
 
-        case 3:
-            printf("/n withdrawal balance");
-            withdrawal(lp);
-            break;
-        case 4:
-            printf("/n exite ");
-            exit(0);
-            break;
-        default:
-            printf("/n invalid choice ");
-            break;
-        }
-    }
-    return 0;
+		case 3:
+			printf("/\n withdrawal balance");
+			withdrawal(lp);
+			break;
+		case 4:
+			printf("\n exite ");
+			i = 0;
+			break;
+		default:
+			printf("\n invalid choice ");
+			break;
+		}
+	}
+	return 0;
 }
 
-void create(node *)
+void create(node *ptr)
+{
+	printf("\n Your account Number is a : 1");
+	ptr->acNo = 1;
+	fflush(stdin);
+	printf("\n Enter a Account holder name :");
+	gets(ptr->name);
+accountType:
+	printf("\n Enter a Account type (1.saving  2. current):");
+	scanf("%d", &ptr->acType);
+	if (ptr->acType != 1 && ptr->acType != 2)
+		goto accountType;
+
+	printf("\n Enter a Ammount (min-500 required:)");
+	scanf("%d", &ptr->ammount);
+	if (ptr->ammount < 500)
+	{
+		printf("Your ammount is less than 500 \n sorry !!! we can not open your account : \n \n");
+	}
+	else
+	{
+		printf("Your account is succesfully creaed !!!! \n \n");
+	}
+}
+void deposite(node *ptr)
 {
 }
-void deposite(node *)
-{
-}
-void withdrawal(node *)
+void withdrawal(node *ptr)
 {
 }
